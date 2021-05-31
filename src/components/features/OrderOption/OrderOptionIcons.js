1812b5f6a1b36d8429e52d05b3af55d894cea343
Michał Lisowiec <miclisowiec@gmail.com>
2021-05-31 19:33:38 +0200

    Add functionality to OrderOptionNumber and OrderOptionIcons

--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {formatPrice} from '../../../utils/formatPrice';
+import styles from './OrderOption.scss';
+import Icon from '../../common/Icon/Icon';
 
-const OrderOptionIcons = () => (
-  <div>OrderOptionCheckboxes</div>
+const OrderOptionIcons = ({values, setOptionValue, required, currentValue}) => (
+  <div className={styles.icon}>
+    {required ? '' : (
+      <div className={styles.icon} setOptionValue=''><Icon name={'times-circle'}>none</Icon></div>
+    )}
+    {values.map(value => (
+      <div
+        className={` ${styles.icon} ${currentValue === value.id ? styles.iconActive : ''}`}
+        key={value.id}
+        onClick={() => setOptionValue(value.id)}>
+        <Icon name={value.icon}/>{value.name} ({formatPrice(value.price)})
+      </div>
+    ))}
+  </div>
 );
 
-export default OrderOptionIcons;
+OrderOptionIcons.propTypes = {
+  values: PropTypes.array,
+  required: PropTypes.bool,
+  currentValue: PropTypes.node,
+  setOptionValue: PropTypes.func,
+};
+  
+export default OrderOptionIcons; 
--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import styles from './OrderOption.scss';
+import {formatPrice} from '../../../utils/formatPrice';
+import PropTypes from 'prop-types';
 
-const OrderOptionNumber = () => (
-  <div>OrderOptionCheckboxes</div>
+const OrderOptionNumber = ({currentValue, setOptionValue, limits, price}) => (
+  <div className={styles.number}>
+    <input 
+      className={styles.inputSmall}
+      type="number"  
+      value={currentValue} 
+      onChange={event => setOptionValue(event.currentTarget.value)} 
+      min={limits.min} 
+      max={limits.max}
+    />
+        ({formatPrice(price)})
+  </div>
 );
+
+OrderOptionNumber.propTypes = {
+  currentValue: PropTypes.node,
+  setOptionValue: PropTypes.func,
+  limits: PropTypes.object,
+  price: PropTypes.string,
+};
+  
 
 export default OrderOptionNumber;  
